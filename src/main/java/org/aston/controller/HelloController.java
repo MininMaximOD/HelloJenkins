ef85c3f3d8bed67698e1a3416335a77cf86323f1
MininMaxim <mininmaxim87@gmail.com>
2022-12-05 11:42:10 +0300

    Первая загрузка проекта

---
+++ src/main/java/org/aston/controller/HelloController.java
@@ -0,0 +1,13 @@
+package org.aston.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/")
+    public String sayHello(){
+        return "hello_jenkins";
+    }
+}
